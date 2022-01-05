6ff7d1ae6a65ba1aa775233ae5a3e54da8dc997c
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-01-05 17:37:29 -0500

    commit tree a9b64aa1361da833993d349b985246c3b13827be

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,19 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+
+    #define the code when the migraiton is run
+    def up 
+    end
+    #code to execute when the migration is rolled back. 
+    def down
+    end
+
+    def change
+        create_table :artists do |t|
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometown
+        end
+    end
+
+end
